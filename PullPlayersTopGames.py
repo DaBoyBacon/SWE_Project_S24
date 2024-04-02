caca8b2b0695dfd408a21456bf69f05dd3d9f88e
joshholston <99925172+joshholston@users.noreply.github.com>
2024-04-02 16:59:40 -0500

    Add files via upload

---
+++ PullPlayersTopGames.py
@@ -0,0 +1,12 @@
+def pullPlayersTopGames(steam_user_id):
+    # Here, would be my code to pull the top 5 most played games 
+    
+    # -access steamAPI and collect top 5 games from players profile 
+    #  that is called in the parameters-
+
+    # for the given steam_user_id from the steamAPI
+    
+    # return top 5 games
+    top_games = ["Game 1", "Game 2", "Game 3", "Game 4", "Game 5"]
+    
+    return top_games
