27557071dcc6b5e859c4f41c2f51b08634aaddd7
Teag0029 <jonathan.a.teague-1@ou.edu>
2024-04-16 16:50:27 -0500

    rework the authlimit timer

--- WorkingCode/BackEnd/PlayerSteamHandling/AuthLimit.py
+++ WorkingCode/BackEnd/PlayerSteamHandling/AuthLimit.py
@@ -1,28 +1,39 @@
-import threading    
+import threading
+import time
 
-class AuthLimiter:
-    num = 0
-    timer = 0
-    
+
+class Timer(threading.Thread):
+
     def __init__(self):
-        self.StartTimer()
+        self._timer_runs = threading.Event()
+        self._timer_runs.set()
+        super().__init__()
 
+    def run(self):
+        while self._timer_runs.is_set():
+            self.timer()
+            time.sleep(self.__class__.interval)
+
+    def stop(self):
+        self._timer_runs.clear()
+
+class someTimer(Timer):
+    interval = 60
+    inc = 0    
+    def __init__(self):
+        self.inc = 0
+        self.interval = 60
+
+    #func to ex
+    def timer(self):
+        print("Clear inc:" + self.inc)
+        self.inc = 0
+        
     def inc(self):
-        self.num += 1
+        self.inc = self.inc + 1
         
-    def StartTimer(self):
-        self.timer = threading.Timer(60, self.ClearNum())
-        self.timer.start()
-        #do stuff
+    def caninc(self) -> bool:
+        return True  if self.inc<60 else False
 
-    def ClearNum(self):
-        self.num = 0
-        print("Resetting the counter")
-        
-    def CanCall(self):
-        if (self.num < 60):  return True
-        else: return False
-        
-c = AuthLimiter()
-c.inc()
-c.inc()
+    def getinc(self):
+        return self.inc
