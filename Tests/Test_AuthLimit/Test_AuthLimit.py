9b2a5b4bffb3d5018975ea211fd4cbc6d25bb7d3
alyxx2214 <81814863+alyxx2214@users.noreply.github.com>
2024-04-23 15:48:41 -0500

    Fix rebase conflicts

--- Tests/Test_AuthLimit/Test_AuthLimit.py
+++ Tests/Test_AuthLimit/Test_AuthLimit.py
@@ -1,8 +1,5 @@
 import unittest
-<<<<<<< HEAD
 import time
-=======
->>>>>>> fa9bebe213e8349a7337ac6a4c4dd263b62b5854
 #just to navigate back to test unit's directory
 import sys
 sys.path.append(sys.path[0]+"\\..\\..\\WorkingCode\\BackEnd\\PlayerSteamHandling")
@@ -14,7 +11,6 @@ class TestSteamNameCheck(unittest.TestCase):
 	def testAuthLimit(self):
 		print("testing")
 		
-<<<<<<< HEAD
 		c = AuthLimit.APILimiter()
 		c.incCt()
 		c.incCt()
@@ -26,18 +22,6 @@ class TestSteamNameCheck(unittest.TestCase):
 		self.assertEqual(c.getinc(), 6)
 		#time.sleep(65)
 		self.assertEqual(c.getinc(), 0)
-=======
-		c = AuthLimit.someTimer()
-		c.start()
-		c.inc()
-		c.inc()
-		c.inc()
-		c.getinc()
-		c.inc()
-		c.inc()
-		c.inc()
-		c.getinc()
->>>>>>> fa9bebe213e8349a7337ac6a4c4dd263b62b5854
 		
 # throw's a fit if you don't put this
 if __name__ == '__main__':
