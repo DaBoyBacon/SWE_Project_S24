550e119d67d17ee3472b1fe6ed1dda773d2e7d2f
alyxx2214 <81814863+alyxx2214@users.noreply.github.com>
2024-04-23 15:46:47 -0500

    Take care of Merge conflicts

--- WorkingCode/BackEnd/PlayerSteamHandling/AuthLimit.py
+++ WorkingCode/BackEnd/PlayerSteamHandling/AuthLimit.py
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import sched, time
-class APILimiter:
-    incCt = 0
-
-    def __init__(self):
-        interval = 60
-        self.my_scheduler = sched.scheduler(time.time, time.sleep)
-        self.my_scheduler.enter(interval, 1, self.reset, (self.my_scheduler,))
-        self.my_scheduler.run()
-
-    def reset(self, scheduler): 
-        # schedule the next call first
-        scheduler.enter(60, 1, self.reset, (scheduler,))
-        print("Doing stuff...")
-        self.incCt = 0
-        # then do your stuff
-
-    def incCt(self):
-        self.incCt = self.incCt + 1
-        
-    def getinc(self):
-        return self.incCt
-        
-    
-=======
 import threading
 import time
 
@@ -63,4 +37,3 @@ class someTimer(Timer):
 
     def getinc(self):
         return self.inc
->>>>>>> fa9bebe213e8349a7337ac6a4c4dd263b62b5854
