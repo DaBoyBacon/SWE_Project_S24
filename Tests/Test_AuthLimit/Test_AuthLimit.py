7e947d9c68cf8b9ce87ad1f78d7b87adaad9b5e4
Teag0029 <jonathan.a.teague-1@ou.edu>
2024-04-23 15:41:56 -0500

    About to do another rework of the two. Watch for next commit

--- Tests/Test_AuthLimit/Test_AuthLimit.py
+++ Tests/Test_AuthLimit/Test_AuthLimit.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 #just to navigate back to test unit's directory
 import sys
 sys.path.append(sys.path[0]+"\\..\\..\\WorkingCode\\BackEnd\\PlayerSteamHandling")
@@ -10,16 +11,17 @@ class TestSteamNameCheck(unittest.TestCase):
 	def testAuthLimit(self):
 		print("testing")
 		
-		c = AuthLimit.someTimer()
-		c.start()
-		c.inc()
-		c.inc()
-		c.inc()
-		c.getinc()
-		c.inc()
-		c.inc()
-		c.inc()
-		c.getinc()
+		c = AuthLimit.APILimiter()
+		c.incCt()
+		c.incCt()
+		c.incCt()
+		self.assertEqual(c.getinc(), 3)
+		c.incCt()
+		c.incCt()
+		c.incCt()
+		self.assertEqual(c.getinc(), 6)
+		#time.sleep(65)
+		self.assertEqual(c.getinc(), 0)
 		
 # throw's a fit if you don't put this
 if __name__ == '__main__':
--- WorkingCode/BackEnd/PlayerSteamHandling/AuthLimit.py
+++ WorkingCode/BackEnd/PlayerSteamHandling/AuthLimit.py
@@ -1,39 +1,24 @@
-import threading
-import time
-
-
-class Timer(threading.Thread):
+import sched, time
+class APILimiter:
+    incCt = 0
 
     def __init__(self):
-        self._timer_runs = threading.Event()
-        self._timer_runs.set()
-        super().__init__()
+        interval = 60
+        self.my_scheduler = sched.scheduler(time.time, time.sleep)
+        self.my_scheduler.enter(interval, 1, self.reset, (self.my_scheduler,))
+        self.my_scheduler.run()
 
-    def run(self):
-        while self._timer_runs.is_set():
-            self.timer()
-            time.sleep(self.__class__.interval)
+    def reset(self, scheduler): 
+        # schedule the next call first
+        scheduler.enter(60, 1, self.reset, (scheduler,))
+        print("Doing stuff...")
+        self.incCt = 0
+        # then do your stuff
 
-    def stop(self):
-        self._timer_runs.clear()
-
-class someTimer(Timer):
-    interval = 60
-    inc = 0    
-    def __init__(self):
-        self.inc = 0
-        self.interval = 60
-
-    #func to ex
-    def timer(self):
-        print("Clear inc:" + self.inc)
-        self.inc = 0
+    def incCt(self):
+        self.incCt = self.incCt + 1
         
-    def inc(self):
-        self.inc = self.inc + 1
-        
-    def caninc(self) -> bool:
-        return True  if self.inc<60 else False
-
     def getinc(self):
-        return self.inc
+        return self.incCt
+        
+    
