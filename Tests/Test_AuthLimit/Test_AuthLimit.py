1ce74c2402ecf697bc8453a6654aaf1ff39fe851
Teag0029 <jonathan.a.teague-1@ou.edu>
2024-04-24 23:25:53 -0500

    reworked timer class, improved testing

--- Tests/Test_AuthLimit/Test_AuthLimit.py
+++ Tests/Test_AuthLimit/Test_AuthLimit.py
@@ -7,44 +7,46 @@ sys.path.append(sys.path[0]+"\\..\\..\\WorkingCode\\BackEnd\\PlayerSteamHandling
 print(sys.path)
 import AuthLimit
 
-class TestSteamNameCheck(unittest.TestCase):
+class TestAuthLimit(unittest.TestCase):
 	def testAuthLimit(self):
 		
 		### This next part
 		### Is testing creating
 		### and incrementing
 
-		print("b4 setting scheduler")
-		t_var = AuthLimit.AuthTimer()
-
-		print("something's happening")
-		t_var.inc()
-		t_var.inc()
-		t_var.inc()
+		#print("b4 setting scheduler") # debug
+		self.t_var = AuthLimit.AuthTimer()
 		
-		self.assertEqual(t_var.getinc(), 3)
+		for num in range(1,self.t_var.maxIncrVal+1):
+			self.assertTrue(self.t_var.canInc())
+			self.assertEqual(self.t_var.getInc(),num)
+		#incr should equal its max val
+		self.assertEqual(self.t_var.getInc(), self.t_var.maxIncrVal)
+		self.assertFalse(self.t_var.canInc())
 
-		print("sleep for 10s")
+		print(f"incr to max: {self.t_var.getInc()}")
+		print(f"sleep for {self.t_var.interval/6}s (some time b4 reset)") #terminal clarification
 
-		time.sleep(10)
-
-		t_var.inc()
-		t_var.inc()
-		t_var.inc()
+		time.sleep(self.t_var.interval/6)
 		
+		#still full
+		self.assertEqual(self.t_var.getInc(), self.t_var.maxIncrVal)
+		self.assertFalse(self.t_var.canInc())
 		
-		self.assertEqual(t_var.getinc(), 6)
+		print(f"Still max: {self.t_var.getInc()}")
 
-		print("Sleep for 60s")
-		time.sleep(60)
+		print(f"Sleep for {self.t_var.interval}s (till reset)") #wait for the timer to reset (60s)
 		
-		self.assertEqual(t_var.getinc(), 0)
+		time.sleep(self.t_var.interval)
 		
-		print(t_var.getinc())
-
+		self.assertEqual(self.t_var.getInc(),0)
+		
+		print(f"Now 0: {self.t_var.getInc()}", )
 		print("Can end :thumbs_up:")
 		#time.sleep(65)
+
 		
+
 # throw's a fit if you don't put this
 if __name__ == '__main__':
     unittest.main()
