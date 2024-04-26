0733b5aba7550b30379426ab4c6a8794743e2c3b
RTW29 <158562481+RTW29@users.noreply.github.com>
2024-04-26 17:38:22 -0500

    Update MatchGamesToScores.py
    Now matches game names from an input list to their scores in the database

--- WorkingCode/BackEnd/GameSteamHandling/MatchGamesToScores.py
+++ WorkingCode/BackEnd/GameSteamHandling/MatchGamesToScores.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar  5 17:48:55 2024
+Created on Fri Apr 26 15:31:01 2024
 
 @author: Guita
 """
-
 import csv
 
-def MatchGamesToScores(csv_file):
-    gameScores = [] # create gameScores array that will contain our final output
-    x: int
+def MatchGamesToScores(csv_file, input_games):
+    gameScores = []
     with open(csv_file, 'r') as file:
-        reader = csv.reader(file) # Read in the CSV file row by row 
-        for row in reader:
-            intList = []
-            for x in range(1, len(row), 1) : # Create a list of int values from the score columns(not the name column because it will be a string)
-                intList.append(int(row[x]))
-            gameScores.append([row[0]]+ intList) # Append the int values to the string name of game, and append that whole list as an element in gameScores
-    return gameScores 
+        reader = csv.reader(file)
+        for row in reader:  # Read in the CSV file row by row 
+            game_name = row[0].strip().lower()  # Convert name in database to lowercase and strip spaces, to prevent formatting error
+            for input_game in input_games: # loop to find scores for each input game name
+                if input_game.strip().lower() in game_name:  # Convert input game name to lowercase and strip spaces, compare to data base
+                    intList = [int(score) for score in row[1:]] # make list of scores for selected game
+                    gameScores.append([row[0]] + intList) # int score elements following game name, append whole list to gameScores
+                    break  # Stop searching for this game once found
+    return gameScores
 
 # Example usage:
-# game_scores = MatchGamesToScore('games_test.csv')
+# game_scores = MatchGamesToScores2('games_test.csv', ["Valheim", "ELDEN RING"])
 # print(game_scores)
-
+# output: [['Valheim', 10, 8, 5], ['New World', 10, 8, 3]]
